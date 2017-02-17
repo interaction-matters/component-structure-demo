07e37560f53be419516c984637f7630f7f4a1a4a
mattdoble <mdoble@epo.org>
2017-02-17 16:03:00 +0100

    Split panel

--- src/Demo.js
+++ src/Demo.js
@@ -1,28 +1,59 @@
 import React, { Component } from 'react';
 import Button from 'components/Button/Button';
-import Snippet from 'components/Snippet/Snippet';
-import QueryInput from 'components/QueryInput/QueryInput';
+import SplitPane from 'react-split-pane';
 
 export default class Demo extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      panelSize: '50%',
+      dualScreen: false
+    }
+  }
+
+  _toggleSize() {
+    this.setState({
+      panelSize: '20%', 
+      dualScreen: true
+    });
+    console.log(this.state.dualScreen);
+  }
+
   render() {
+
     return (
     	<div>
-        <h4><i className="icon-dossier-files"></i> Buttons Demo</h4>
-        <hr />
-      	<Button type="default">Default</Button>&nbsp;
-      	<Button type="primary">Primary</Button>&nbsp;
-      	<Button type="success">Success</Button>&nbsp;
-      	<Button type="info">Info</Button>&nbsp;
-      	<Button type="warning">Warning</Button>&nbsp;
-      	<Button type="danger">Danger</Button>
-      	<hr />
-      	<Button type="primary" size="small">Small Button</Button>&nbsp;
-      	<Button type="warning">Button</Button>&nbsp;
-      	<Button type="default" size="large">Large Button</Button>
-      	<hr />
-        <Snippet />
-        <hr />
-        <QueryInput />
+        <SplitPane split="vertical" defaultSize={this.state.panelSize}>
+            <div style={{padding:'1em'}}>
+              <h4><i className="icon-dossier-files"></i> Buttons Demo</h4>
+              <hr />
+              <Button type="default">Default</Button>&nbsp;
+              <Button type="primary">Primary</Button>&nbsp;
+              <Button type="success" onAddClick={this._toggleSize.bind(this)}>Success</Button>&nbsp;
+              <Button type="info">Info</Button>&nbsp;
+              <Button type="warning">Warning</Button>&nbsp;
+              <Button type="danger">Danger</Button>
+              <hr />
+              <Button type="primary" size="small">Small Button</Button>&nbsp;
+              <Button type="warning">Button</Button>&nbsp;
+              <Button type="default" size="large">Large Button</Button>
+            </div>
+            {( this.state.dualScreen ?
+                <SplitPane split="vertical" defaultSize="50%">
+                  <div style={{padding:'1em'}}>
+                    more content
+                  </div>
+                  <div style={{padding:'1em'}}>
+                    Even more content
+                  </div>
+                </SplitPane>
+                :
+                <div style={{padding:'1em'}}>
+                  more content
+                </div>
+            )};
+        </SplitPane>
       </div>
     );
   }
--- src/components/QueryInput/QueryInput.js
+++ src/components/QueryInput/QueryInput.js
@@ -2,38 +2,46 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import styles from './QueryInput.scss';
 
+
+const activeClass = {
+	border: "1px solid red"
+}
+
+
 export default class QueryInput extends Component {
 
 	constructor(props) {
 	    super(props);
 	    this.state = {
-	    	query: 'inactive'
+	    	activeArea: ''
 		}
 	}
 
-	clickHandler(e) {
-		e.currentTarget.style.border = "1px solid red";
-		console.log(e.target);
-		console.log(e.currentTarget);
-		console.log(ReactDOM.findDOMNode(this));
+	toggleActiveArea(e) {
+		let id = e.target.getAttribute("data-target");
+		this.setState({activeArea: id});
+		let activeElement = document.getElementById(id);
+		activeElement.setAttribute("style", activeClass);
+
+		console.log("activeElement:" + activeElement);
+		console.log("id: " + id);
+		console.log("active state: " + this.state.activeArea);
 	}
 
   	render() {
 
-  		let activeClass = {
-  			border: "1px solid red"
-  		}
-
 	    return (
 			<div className={styles.query}>
-				<span className={styles.term}>Pizza<span style={{color: "magenta"}}>+</span></span> 
+				<span className={styles.term} data-target="term1" id="term1">Pizza<span style={{color: "magenta"}}>+</span></span> 
 				<span className={styles.proximity} style={{color: "blue"}}>3d</span>
-				<span className={styles.brackets} >
-					<span className={styles.bracket} style={{color: "green"}} onMouseEnter={this.clickHandler.bind(this)}>(</span>
-						<span className={styles.term}>oregano<span style={{color: "magenta"}}>?</span></span>
-						<span className={styles.proximity} style={{color: "blue"}}>2d</span>
-						<span className={styles.term}>basil<span style={{color: "magenta"}}>+</span></span>
-					<span className={styles.bracket} style={{color: "green"}} onMouseEnter={this.clickHandler.bind(this)}>)</span>
+				<span className={styles.brackets} id="brackets">
+					<span className={styles.bracket} data-target="brackets" style={{color: "green"}} 
+						  onMouseEnter={this.toggleActiveArea.bind(this)}
+						  >(</span>
+						<span className={styles.term} data-target="term2" id="term2" onMouseEnter={this.toggleActiveArea.bind(this)}>oregano<span style={{color: "magenta"}}>?</span></span>
+						<span className={styles.proximity} style={{color: "blue"}} onMouseEnter={this.toggleActiveArea.bind(this)}>2d</span>
+						<span className={styles.term} data-target="term3" id="term3">basil<span style={{color: "magenta"}} onMouseEnter={this.toggleActiveArea.bind(this)}>+</span></span>
+					<span className={styles.bracket} style={{color: "green"}} onMouseEnter={this.toggleActiveArea.bind(this)}>)</span>
 				</span>
 			</div>
 			)
